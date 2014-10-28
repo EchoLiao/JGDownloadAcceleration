24a2feaedcc394766e776adea875baa9bc04fc77
Echo Liao <nuoerlz@gmail.com>
2014-10-28 13:13:53 +0800

    Remove unuse code

--- JGDownloadAcceleration Classes/JGDownloadOperation.h
+++ JGDownloadAcceleration Classes/JGDownloadOperation.h
@@ -10,12 +10,10 @@
 
 @interface JGDownloadOperation : NSOperation
 
-//optional settings
 @property (nonatomic, assign) NSUInteger tag; //default 0
 @property (nonatomic, assign) NSUInteger maximumNumberOfConnections; //default 6
 @property (nonatomic, assign) NSUInteger retryCount; //default maximumNumberOfConnections/2
 
-//readonly
 @property (nonatomic, strong, readonly) NSURLRequest *originalRequest;
 @property (nonatomic, strong, readonly) NSString *destinationPath;
 @property (nonatomic, strong,         ) NSString *finalDestinationPath;
@@ -28,13 +26,11 @@
 - (unsigned long long)downloadedLength;
 - (double)averageSpeed; // in kBytes/s
 
-//custom init methods
 - (instancetype)initWithURL:(NSURL *)url destinationPath:(NSString *)path allowResume:(BOOL)resume;
 - (instancetype)initWithRequest:(NSURLRequest *)request destinationPath:(NSString *)path allowResume:(BOOL)resume;
 
-- (void)cancelAndClearFiles; //cancel the operation and remove the partial file as well as the metadata file
+- (void)cancelAndClearFiles;
 
-//Delegate blocks
 - (void)setCompletionBlockWithSuccess:(void (^)(JGDownloadOperation *operation))success failure:(void (^)(JGDownloadOperation *operation, NSError *error))failure;
 - (void)setDownloadProgressBlock:(void (^)(NSUInteger bytesRead, unsigned long long totalBytesReadThisSession, unsigned long long totalBytesRead, unsigned long long totalBytesExpectedToRead, NSUInteger tag))block;
 - (void)setOperationStartedBlock:(void (^)(NSUInteger tag, unsigned long long totalBytesExpectedToRead))block;
